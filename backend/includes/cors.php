6e99a7c094cbac33ab3a9d6d08e22c5cfc1d0518
AfonsoRuasPT <afonsotruas@gmail.com>
2026-03-14 17:19:33 +0000

    Backend Cors | bugCorrected

--- backend/includes/cors.php
+++ backend/includes/cors.php
@@ -5,6 +5,8 @@ $allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
+    'http://localhost:3002',
+    'http://127.0.0.1:3002',
     'https://wild-log.com',
     'https://rh360.pt',
 ];
