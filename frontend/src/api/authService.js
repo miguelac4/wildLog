5ddcc25021b8360b78694be15ff687f1f0f6329b
AfonsoRuasPT <afonsotruas@gmail.com>
2026-03-30 21:21:40 +0100

    "Reverte authService.js para o estado do commit 66d4662"

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -58,22 +58,6 @@ export const authService = {
                 confirm_password
             })
         });
-    },
-
-    updateProfile: async (name, description) => {
-        return apiFetch("/account/edit_account_info.php", {
-            method: "POST",
-            body: JSON.stringify({
-                name,
-                description
-            })
-        });
-    },
-
-    getProfile: async () => {
-        return apiFetch("/account/get_account.php", {
-            method: "GET"
-        });
     }
 
 };
